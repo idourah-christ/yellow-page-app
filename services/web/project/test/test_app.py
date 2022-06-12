90f9ec64de0835ad1f4c1ee96e8cf927d030234a
Bahamboula idourah <idourah96@gmail.com>
2022-06-12 16:24:41 +0200

    test

--- services/web/project/test/test_app.py
+++ services/web/project/test/test_app.py
@@ -7,14 +7,5 @@ import pytest
 def test_app(app):
     assert isinstance(app, flask.Flask)
 
-def test_create_category(db, session):
-    """
-    test creation of a category in the database
-    """
-    category = Category(name="Restaurant")
-    db.session.add(category)
-    db.session.commit()
-     
-    cat_obj = Category.query.filter_by(name="Restaurant").one()
-    assert cat_obj is not None
+
     
